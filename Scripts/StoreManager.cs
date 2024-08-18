00505763683ee3f2d7755814ce6fa2b70f474f7c
1023941311414 <jake.widmann@outlook.com>
2024-08-18 16:27:35 -0400

    Store and randomization in progress.

--- Scripts/StoreManager.cs
+++ Scripts/StoreManager.cs
@@ -2,7 +2,23 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public class StoreManager
+public class StoreConfiguration
 {
-    
+    private VirusItem[] viruses;
+    private PlantInfo[] plants;
+    public StoreConfiguration(VirusItem[] viruses, PlantInfo[] plants)
+    {
+        this.viruses = viruses;
+        this.plants = plants;
+    }
+
+    public VirusItem[] GetVirusItems()
+    {
+        return viruses;
+    }
+
+    public PlantInfo[] GetPlantInfos()
+    {
+        return plants;
+    }
 }
--- Scripts/StoreUI.cs
+++ Scripts/StoreUI.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class StoreUI : Control
 {
+	[Export] private VirusItem[] virusPresets;
+
     public override void _Ready()
     {
         
@@ -12,4 +14,34 @@ public partial class StoreUI : Control
 	{
 		Visible = !Visible;
 	}
+
+	private StoreConfiguration RandomizeStore(int numViruses, int numPlants)
+    {
+        VirusItem[] viruses = new VirusItem[numViruses];
+        PlantInfo[] plants = new PlantInfo[numPlants];
+        for (int i = 0; i < numViruses; i++)
+        {
+            viruses[i] = virusPresets[GD.RandRange(0, virusPresets.Length - 1)];
+        }
+        for (int i = 0; i < numPlants; i++)
+        {
+            PlantInfo plant = new PlantInfo();
+            int speciesIndex = GD.RandRange(0, 2);
+            if (speciesIndex == 0)
+            {
+                plant.species.species = Species.PlantSpecies.YellowTroutLily;
+                plant.species.texture = (Texture2D) GD.Load("res://Art/yellow trout lily _test.png");
+            } else if (speciesIndex == 1)
+            {
+                plant.species.species = Species.PlantSpecies.JuniperBerries;
+                plant.species.texture = (Texture2D) GD.Load("res://Art/juniper berries _test.png");
+            } else if (speciesIndex == 2)
+            {
+                plant.species.species = Species.PlantSpecies.CinnamonFern;
+                plant.species.texture = (Texture2D) GD.Load("res://Art/cinnamon fern _test.png");
+            }
+            plants[i] = plant;
+        }
+        return new StoreConfiguration(viruses, plants);
+    }
 }
